1a2af7a9c38b40a012274f48670cfe9ecdc82c7b
AceIsAwesome <stmjv02@moravian.edu>
2015-04-23 13:33:28 -0400

    classes for towers and bullets

---
+++ src/model/entities/Bullet.java
@@ -0,0 +1,20 @@
+package model.entities;
+
+import model.World;
+import pathfinding.NavGraph;
+
+public class Bullet extends Entity {
+
+	public Bullet(double initX, double initY, double direction, double r,
+			World myWorld, NavGraph graph) {
+		super(initX, initY, direction, r, myWorld, graph);
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public void update(int delta) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
---
+++ src/model/entities/Tower.java
@@ -0,0 +1,20 @@
+package model.entities;
+
+import model.World;
+import pathfinding.NavGraph;
+
+public class Tower extends Entity {
+
+	public Tower(double initX, double initY, double direction, double r,
+			World myWorld, NavGraph graph) {
+		super(initX, initY, direction, r, myWorld, graph);
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public void update(int delta) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
